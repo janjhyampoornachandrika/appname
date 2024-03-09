dee1786fe7aaf5a66e8bdbd4b03bf00360cb720f
npx <chandrikajanjhyam@gmail.com>
2024-03-09 15:50:29 +0530

    msg

---
+++ src/App14.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FaHome,FaBookmark}from "react-icons/fa";
+export default function App14() {
+  return (
+    <div>
+   <p><FaHome/>Home</p>
+   <p><FaBookmark/>Bookmark</p>
+  </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App9 from "./App9";
+import App15 from "./App15";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render
 
 (
   <React.StrictMode>
-    <App9/>
+    <App15/>
   </React.StrictMode>  
  
 );
